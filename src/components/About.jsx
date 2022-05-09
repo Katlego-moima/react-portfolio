078bfdca2ba893c60404469ddb74c2f57adcd2ea
katlego <moima@thedigitalacademy.co.za>
2022-05-09 13:39:03 +0200

    grid for the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,20 @@ const About = () => {
           </div>
           <div></div>
         </div>
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right text-4xl font-bold ">
+            <p>Hi. I'm Kat, nice to meet you. Please take a look around.</p>
+          </div>
+          <div>
+            <p>
+              I am self-driven and i like to learn new things related to coding.
+              i completed my diploma in IT, specializing in Software
+              Development. and currently doing an internship in Software
+              Development mostly focused in Front-End development. I would like
+              to gain more necessary experience in the field.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
